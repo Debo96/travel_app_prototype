f0e7490f45a18aba0d25f4ccea1333269917caa1
Debo96 <debpriyopaul96@gmail.com>
2020-06-24 16:04:59 +0530

    Add files via upload
    Latest add:- completed till registration page of app

--- app.py
+++ app.py
@@ -10,7 +10,19 @@ from kivy.uix.boxlayout import BoxLayout
 import kivy.uix.button as kb
 from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
-
+import smtplib
+import random
+import string
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+import threading
+from threading import Thread,Event
+from multiprocessing import Process
+import multiprocessing
+import time
+import threading
 
 
 
@@ -49,10 +61,17 @@ Builder.load_string("""
             root.manager.current = 'register'
 <register>:
     last_email_text_input: email
+    last_otp_text_input: OTP
+    lbl:my_label
     Label:
         text:"Register"
         pos:0,200
     
+    Label:
+        id: my_label
+        color: [1, 0, 0, 1] 
+        
+        
     Label:
         text:"Email"
         pos:-180,150
@@ -96,10 +115,8 @@ Builder.load_string("""
         size_hint: (.3,.1)
         pos:(280,180)
         width: 200
-        on_press: 
-            root.manager.transition.direction = 'left' 
-            root.manager.transition.duration = 1 
-            root.manager.current = 'account'
+        on_press: root.press()   
+        on_press: root.afterpress()
 <details>:
     Label:
         text:"Enter Credentials"
@@ -219,10 +236,109 @@ class register(Screen):
     pass
     def __init__(self, **kwargs):
         super(register, self).__init__(**kwargs)
+        
     def verify(self):
         self.email=self.last_email_text_input.text
+        self.generate_pass = ''.join([random.choice( string.ascii_uppercase +string.ascii_lowercase + string.digits)  for n in range(10)])
+        if '@' in self.email:
+            fromaddr = "babipaul135@gmail.com"
+            toaddr = self.email
+            msg = MIMEMultipart() 
+            msg['From'] = fromaddr 
+            msg['To'] = toaddr 
+            msg['Subject'] = "Thanks for choosing us"
+            message='Hello, this is your authentication OTP:-{}'.format(self.generate_pass)
+            msg.attach(MIMEText(message))
+            s = smtplib.SMTP('smtp.gmail.com', 587) 
+            s.ehlo()
+            s.starttls() 
+            s.login(fromaddr, "babipaljoker") 
+            text = msg.as_string() 
+            s.sendmail(fromaddr, toaddr, text) 
+            s.quit() 
+            self.lbl.text='OTP sent successfully!!'
+            Clock.schedule_once(self.reset_label, 11)
+            
+            
+        else:
+            self.lbl.text='Incorrect email id!!'
+           
+            
+    def reset_label(self,*args):
+        self.lbl.text=''
+        
+
+    
+    def auth(self, *args):
+        self.email_otp=self.generate_pass
+        self.otp=self.last_otp_text_input.text
+        
+        if self.otp==self.email_otp:
+            self.changeScreen()
+            
+        
+            
+        else:
+            self.lbl.text='OTP is incorrect'
+        
+        
+        
+            
+                
+            
+            
+            
+           
+                
+            
+            
+            
+            
+            
+            
+            
+            
+              
+        
+           
+              
+            
+                
+       
+        
+        
+            
+        
+        
+      
+                
+               
+        
+    def changeScreen(self, *args):
+        
+        #now switch to the screen next
+        self.parent.current = "account"
+    
+    def invalid(self,*args):
+        
+            
+        self.lbl.text='OTP is not valid anymore'
+        self.parent.current='register'
+    
+    def press(self):
+        timer = threading.Timer(1.0, self.auth) 
+        timer.start()
+        if self.lbl.text=='OTP is not valid anymore':
+            timer.cancel()
+        
+    def afterpress(self):
+        timer = threading.Timer(25.0,self.invalid)
+        timer.start()
+       
+   
     
         
+
     
   
 class details(Screen):
@@ -263,6 +379,7 @@ sm.add_widget(intro(name="intro"))
 class App(App):
     def build(self):
         return sm
+    
         
 App().run()
  
